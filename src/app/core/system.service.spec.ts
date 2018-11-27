436690e473f55ca386d632609c28d01257b616c2
Sébastien Gallou <sebastien.gallou@gmail.com>
2018-11-28 00:31:21 +0100

    Fix test

--- src/app/core/system.service.spec.ts
+++ src/app/core/system.service.spec.ts
@@ -1,9 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { SystemService } from './system.service';
+import {SystemService} from './system.service';
+import {RestServerService} from './restserver.service';
 
 describe('SystemService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{provide: RestServerService}]
+  }));
 
   it('should be created', () => {
     const service: SystemService = TestBed.get(SystemService);
